96e5ea6053bc219f8b6be91efe0b04f2da974266
chithrakundarconnect-sys <chithrakundar.connect@gmail.com>
2026-03-02 14:24:51 +0530

    Fixed CORS to allow frontend connection

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from predictor import predict_next_period, days_remaining
 
 app = FastAPI()
+origins = [
+    "http://localhost:9002",
+    "http://127.0.0.1:9002",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
